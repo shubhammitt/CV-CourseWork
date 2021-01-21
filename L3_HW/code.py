e5ec629b12b1e5be275cd46e7c059b4659f61757
Shubham Mittal <shubham18101@iiitd.ac.in>
2021-01-21 11:23:57 +0530

    Extracted background frames using numpy-HW3

--- L3_HW/code.py
+++ L3_HW/code.py
@@ -25,54 +25,61 @@ def extract_frames(video_name):
 		number_of_frames += 1
 
 
-def median(List):
-	return sorted(List)[len(List) // 2]
-
-
-def get_background_frame(background_strategy):
-
-	rows, cols, channels = extracted_frames[0].shape
-
-	background_frame = np.zeros((rows , cols, channels), np.uint8)
-
-	for row in range(rows):
-		for col in range(cols):
-			for channel in range(channels):
-				pixels = []
-				
-				for frame in extracted_frames:
-					pixels.append(frame[row][col][channel])
-
-				new_pixel = 0
-
-				if background_strategy == 'mean':
-					new_pixel = mean(pixels)
-				elif background_strategy == 'mode':
-					new_pixel = mode(pixels)
-				else:
-					new_pixel = median(pixels)
-
-				background_frame[row][col][channel] = int(new_pixel)
-				
-
-	cv2.imshow('background_frame', background_frame)
-	cv2.waitKey()
-	cv2.destroyAllWindows()
-	return background_frame
-
-# def get_background_frame1(background_strategy):
-
-# 	if background_strategy == 'mean':
-# 		background_frame = np.mean(np.array(extracted_frames), dtype=np.int64, axis=0)
-# 	elif background_strategy == 'mode':
-# 		background_frame = np.mode(np.array(extracted_frames), dtype=np.int64, axis=0)
+# def median(List):
+# 	List = sorted(List)
+# 	n = len(List)
+# 	if n % 2 == 1:
+# 		return List[n // 2]
 # 	else:
-# 		background_frame = np.median(np.array(extracted_frames), dtype=np.int64, axis=0)
+# 		return (int(List[n // 2 - 1]) + List[n // 2]) // 2
+
+
+
+# def get_background_frame(background_strategy):
+
+# 	rows, cols, channels = extracted_frames[0].shape
+
+# 	background_frame = np.zeros((rows , cols, channels), np.uint8)
+
+# 	for row in range(rows):
+# 		for col in range(cols):
+# 			for channel in range(channels):
+# 				pixels = []
+				
+# 				for frame in extracted_frames:
+# 					pixels.append(frame[row][col][channel])
+
+# 				new_pixel = 0
+
+# 				if background_strategy == 'mean':
+# 					new_pixel = mean(pixels)
+# 				elif background_strategy == 'mode':
+# 					new_pixel = mode(pixels)
+# 				else:
+# 					new_pixel = median(pixels)
+
+# 				background_frame[row][col][channel] = int(new_pixel)
 
 # 	cv2.imshow('background_frame', background_frame)
 # 	cv2.waitKey()
 # 	cv2.destroyAllWindows()
 # 	return background_frame
+
+def get_background_frame(background_strategy):
+
+	if background_strategy == 'mean':
+		background_frame = np.mean(np.array(extracted_frames), axis=0)
+	elif background_strategy == 'mode':
+		background_frame = np.apply_along_axis(mode, 0, np.array(extracted_frames))
+	else:
+		background_frame = np.median(np.array(extracted_frames), axis=0)
+
+	background_frame = background_frame.astype(np.uint8)
+
+	cv2.imshow('background_frame', background_frame)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
+	return background_frame
 
 
 if __name__ == '__main__':
