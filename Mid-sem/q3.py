5ca1320936ec435b9c3deb8dc8d9b6de1ad36d71
Shubham Mittal <shubham18101@iiitd.ac.in>
2021-02-27 21:22:36 +0530

    q3 working-3min

--- Mid-sem/q3.py
+++ Mid-sem/q3.py
@@ -4,28 +4,35 @@ import copy
 import numpy as np
 import itertools, math
 import time
+from multiprocessing import Pool
+
+rgb_frequency = {}
+saliency = {}
+
+num_pixels = 0
+
+def process_pixel(pixel1):
+	pixel1_b, pixel1_g, pixel1_r = map(int, pixel1.split())
+	chebyshev_distance = 0		
+	for pixel2, frequency2 in rgb_frequency.items():
+		pixel2_b, pixel2_g, pixel2_r = map(int, pixel2.split())
+		chebyshev_distance += frequency2 * max(abs(pixel1_r - pixel2_r), abs(pixel1_b - pixel2_b), abs(pixel1_g - pixel2_g))
+
+	return (pixel1, chebyshev_distance)
+		
 
 def calculate_saliency(rgb_frequency, originalImage):
-	height, width = originalImage.shape[:2]
-	num_pixels = height * width
-	saliency = {}
-	c = 0
-
-	for pixel1, frequency1 in rgb_frequency.items():
-		pixel1_b, pixel1_g, pixel1_r = map(int, pixel1.split())
-		chebyshev_distance = 0		
-		for pixel2, frequency2 in rgb_frequency.items():
-			pixel2_b, pixel2_g, pixel2_r = map(int, pixel2.split())
-			chebyshev_distance += frequency2 * max(abs(pixel1_r - pixel2_r), abs(pixel1_b - pixel2_b), abs(pixel1_g - pixel2_g))
-		
-		saliency[pixel1] = (chebyshev_distance) / num_pixels
-
-	# print(saliency)
-	max_sal = max(saliency.values())
-	print(max_sal)
-	for i in saliency:
-		saliency[i] = saliency[i] / max_sal
+	global saliency
+	pool = Pool(4)                         # Create a multiprocessing Pool
+	pixel_saliency = pool.map(process_pixel, rgb_frequency.keys())
+	max_sal = 0
+	for x, y in pixel_saliency:
+		max_sal = max(max_sal, y)
+	
+	for x, y in pixel_saliency:
+		saliency[x] = y / max_sal
 	return saliency
+
 
 def map_saliency(originalImage, saliency):
 	height, width = originalImage.shape[:2]
@@ -33,17 +40,17 @@ def map_saliency(originalImage, saliency):
 	for i in range(height):
 		for j in range(width):
 			image[i][j] = saliency[str(originalImage[i][j][0]) + " " + str(originalImage[i][j][1]) + " " + str(originalImage[i][j][2])]
-			# image[i][j] = 1
 	
 	image=np.float64(image)
-	# print(image)
 	# cv2.imshow('saliency', image)
 	# cv2.waitKey()
 	# cv2.destroyAllWindows()
 
 
 def find_rgb_frequency(originalImage):
+	global rgb_frequency, num_pixels
 	height, width = originalImage.shape[:2]
+	num_pixels = height * width
 	rgb_frequency = {}
 	for i in range(height):
 		for j in range(width):
@@ -56,13 +63,13 @@ def find_rgb_frequency(originalImage):
 	return rgb_frequency
 
 
-
 def main(image_path):
 	originalImage = cv2.imread(image_path)
-	originalImage = cv2.resize(originalImage, (100, 100))
+	# originalImage = cv2.resize(originalImage, (100, 100))
 	rgb_frequency = find_rgb_frequency(originalImage)
 	saliency = calculate_saliency(rgb_frequency, originalImage)
 	map_saliency(originalImage, saliency)
+
 
 
 if __name__ == '__main__':
@@ -72,3 +79,4 @@ if __name__ == '__main__':
 
 	image_path = sys.argv[1]
 	main(image_path)
+	
